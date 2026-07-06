9703be8df6b58c29fd6c1384ccb2ce81f2455209
Hardik Grover <121461672+hardikOG@users.noreply.github.com>
2026-07-07 03:52:45 +0530

    Time: 17 ms (93.35%), Space: 19.2 MB (82.33%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -6,16 +6,14 @@ class Solution(object):
         :rtype: int
         """
         n = len(cardPoints)
-        if n == k:
-            return sum(cardPoints)
-        left_sum, right_sum = 0, 0 
-        for i in range(0,k):
-            left_sum += cardPoints[i]
-        maxi = left_sum
-        right_ind = n-1
-        for i in range(k-1,-1,-1):
-            left_sum -= cardPoints[i]
-            right_sum += cardPoints[right_ind]
-            maxi = max(maxi, left_sum + right_sum)
-            right_ind -=1
-        return maxi
+        l = 0
+        r = n-k
+        total = sum(cardPoints[r:])
+        res = total
+        for r in range(r, n):
+            total -= cardPoints[r]
+            total += cardPoints[l]
+            res = max(total, res)
+            l+=1
+        return res
+        
