386ba70d6770ddf7d6560bc28fe182d75607725d
Hardik Grover <121461672+hardikOG@users.noreply.github.com>
2026-07-31 23:55:19 +0530

    Time: 220 ms (99.47%), Space: 12.5 MB (99.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -5,14 +5,9 @@ class Solution(object):
         :type coins: List[int]
         :rtype: int
         """
-        n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n+1)]
-        for i in range(n+1): #base case, to make amount 0
-            dp[i][0] = 1
-        for i in range(1, n+1):
-            for j in range(1, amount+1):
-                dp[i][j] = dp[i-1][j]
-                if coins[i-1] <= j:
-                    dp[i][j] += dp[i][j-coins[i-1]]
-                    
-        return dp[n][amount]
+        C = [0] * (amount + 1)
+        C[0] = 1
+        for coin in coins:
+            for a in xrange(coin, amount + 1):
+                C[a] += C[a - coin]
+        return C[amount]  
