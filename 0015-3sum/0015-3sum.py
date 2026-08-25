571612276dc1fd6a2b481065105574e3079bb7e5
Hardik Grover <121461672+hardikOG@users.noreply.github.com>
2026-08-25 06:57:12 +0530

    Time: 771 ms (37.91%), Space: 18.3 MB (81.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,28 @@
 class Solution(object):
     def threeSum(self, nums):
-        nums.sort()
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         res = []
-
-        for i in range(len(nums) - 2):
-            if nums[i] > 0:
-                break
-
+        n = len(nums)
+        nums.sort()
+        for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
-            left, right = i + 1, len(nums) - 1
-
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif total < 0:
-                    left += 1
+            left = i+1
+            right = n-1
+            
+            while left<right: 
+                summ = nums[i] + nums[left] + nums[right]
+                if summ == 0:
+                    res.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    while left<right and nums[left] == nums[left-1]:
+                        left+=1
+                elif summ < 0:
+                    left+=1
                 else:
-                    right -= 1
-
+                    right-=1
+            
         return res
