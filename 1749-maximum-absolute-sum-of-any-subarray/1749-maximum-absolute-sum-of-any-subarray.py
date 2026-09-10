90931581df81bd3c9d0af015282e0d0ece9ab39e
Hardik Grover <121461672+hardikOG@users.noreply.github.com>
2026-09-10 14:16:14 +0530

    Time: 44 ms (97.18%), Space: 20 MB (67.53%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxAbsoluteSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum = 0
+        pos_window = 0
+        neg_window =0
+        n = len(nums)
+        left = 0
+        for right in range(n):
+            pos_window += nums[right]
+            neg_window += nums[right]
+            if pos_window < 0:
+                pos_window = 0
+                left = right + 1
+            elif neg_window > 0:
+                neg_window = 0
+                left = right + 1
+            max_sum = max(max_sum, abs(pos_window), abs(neg_window))
+        return max_sum
