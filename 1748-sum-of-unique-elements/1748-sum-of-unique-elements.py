40961fe3275f6b1dbeb097679dbf10370095b1c6
Hardik Grover <121461672+hardikOG@users.noreply.github.com>
2026-09-18 15:10:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (17.35%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,15 +1,14 @@
 class Solution(object):
     def sumOfUnique(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         freq = {}
-        
-        for num in nums:
-            freq[num] = freq.get(num, 0)+1
         total = 0
+
         for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
             if freq[num] == 1:
-                total+=num
-        return total 
+                total += num
+            elif freq[num] == 2:
+                total -= num
+
+        return total
